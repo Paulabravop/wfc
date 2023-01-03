e003c4c9f5fa577eb1d7fb1fef115297cdfb764c
Paula Bravo <paula.bravo@ucuenca.edu.ec>
2023-01-02 19:12:58 -0500

    Aparición de azulejos

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ const RETICULA= 4;
 const azulejos =[];
 const NA = 12; //numero de azulejos
 
+let ancho;  //ancho de celdas
+let alto; //altura de celdas
+
+
 const reglas =[
   //reglas de los bordes de cada azulejo
 {
@@ -92,28 +96,65 @@ const reglas =[
 },
 ];
 function preload(){
-  for(let i =0; i < NA; i++){
+  for(let i = 0; i < NA; i++){
     azulejos[i] = loadImage(`azulejos/tile${i}.png`);
   }
 }
-
 function setup() {
   createCanvas(1080, 1080);
 
+  ancho = width / RETICULA;
+  alto = height / RETICULA;
 
-  let opcionesI = []
-  for(let i = 0 ; i > azulejos.length; i++ ){
+  let opcionesI =[]
+
+  for(let i = 0; i < azulejos.length; i++){
     opcionesI.push(i);
   }
-  for(let i=0; i < RETICULA * RETICULA; i++){
-    celdas[i]= {
-      colapsada: false,
-      opciones: opcionesI 
-    }
-    print(celdas);
-  }
-}
 
-function draw() {
-  circle(mouseX, mouseY, 20);
+  for(let i = 0; i < RETICULA * RETICULA; i ++){
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    }
+  }
+
 }
+function draw() {
+  background(111);
+  const celdasDisponibles = celdas.filter((celda) =>{ 
+  return celda.colapsada == false; 
+  });
+
+  if(celdasDisponibles.length > 0){
+    celdasDisponibles.sort((a, b) =>{
+      return a.opciones.length - b.opciones.length;
+    });
+
+    const celdasPorColapsar = celdasDisponibles.filter((celda)=>{
+      return (
+        celda.opciones.length == celdasDisponibles[0].opciones.length);
+    });
+    
+    
+    const celdaSeleccionada = random (celdasPorColapsar);
+    celdaSeleccionada.colapsada = true;
+
+    const opcionSeleccionada = random(celdaSeleccionada.opciones);
+    celdaSeleccionada.opciones = [opcionSeleccionada];
+
+    
+    for(let x = 0; x < RETICULA; x++){
+      for(let y = 0; y < RETICULA; y++){
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+        if(celdaActual.colapsada){
+           
+          image (azulejos[celdaActual.opciones[0]], x, y);
+
+          }
+        }
+      }
+      noLoop();
+    }
+  }
